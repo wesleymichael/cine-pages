d7f0166593c48602dbe16de56d714c2428ff5ae5
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-29 10:49:14 -0300

    feat: validations when signing up

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -27,50 +27,74 @@ export function Register(){
             alert('Erro, tente novamente');
         });
     }
+
+    function validateEmail(email) {
+        const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        return emailRegex.test(email);
+      }
     
+      function validateUsername(username) {
+        const usernameRegex = /^[a-zA-Z0-9_]+$/;
+        return usernameRegex.test(username);
+      }
+    
+      function validateImgUrl(imgUrl) {
+        const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+        return urlRegex.test(imgUrl);
+      }
+
+
     return (
         <Container>
-            <Form onSubmit={handleSubmit}>
-                <Input
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    onChange={handleChange}
-                    value={form.email}
-                    disabled={isLoading}
-                    required
-                />
-                <Input
-                    type="text"
-                    placeholder="Nome do usuário"
-                    name="username"
-                    onChange={handleChange}
-                    value={form.username}
-                    disabled={isLoading}
-                    required
-                />
-                <Input
-                    type="password"
-                    placeholder="Senha"
-                    name="password"
-                    onChange={handleChange}
-                    value={form.password}
-                    disabled={isLoading}
-                    required
-                />
-                <Input
-                    type="text"
-                    placeholder="Foto de perfil"
-                    name="img"
-                    onChange={handleChange}
-                    value={form.img}
-                    disabled={isLoading}
-                    required
-                />
-                <Button type="submit" disabled={isLoading}>
-                    { isLoading ? "Loading..." : "Cadastrar" }
-                </Button>
-            </Form>
+             <Form onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          value={form.email}
+          disabled={isLoading}
+          required
+          pattern={validateEmail(form.email) ? undefined : "[^\\s]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"}
+          title="Insira um email válido"
+        />
+        <Input
+          type="text"
+          placeholder="Nome do usuário"
+          name="username"
+          onChange={handleChange}
+          value={form.username}
+          disabled={isLoading}
+          required
+          pattern={validateUsername(form.username) ? undefined : "[a-zA-Z0-9_]+"}
+          title="Username deve conter apenas letras, números e underline"
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          name="password"
+          onChange={handleChange}
+          value={form.password}
+          disabled={isLoading}
+          required
+          minLength={6}
+          title="A senha deve ter pelo menos 6 caracteres"
+        />
+        <Input
+          type="text"
+          placeholder="Foto de perfil"
+          name="img"
+          onChange={handleChange}
+          value={form.img}
+          disabled={isLoading}
+          required
+          pattern={validateImgUrl(form.img) ? undefined : "^(ftp|http|https):\\/\\/[^ \"].+$"}
+          title="Insira uma URL válida"
+        />
+        <Button type="submit" disabled={isLoading}>
+          {isLoading ? "Loading..." : "Cadastrar"}
+        </Button>
+      </Form>
             <StyledLink to="/signin">
                 Já tem uma conta? Faça login!
             </StyledLink>
